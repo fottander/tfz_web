548870ed2dcd3b0d996d6805f9ef91b2ea5cfe7d
felixottander <felix.ottander@yahoo.com>
2017-05-25 18:22:59 +0200

    Modifies seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 News.delete_all
 
 News.create(title: "Awesome travel insights", content: "Let me tell you about my trip to British Columbia")
+News.create(title: "Great skiing in Aspen", content: "Let me tell you about my trip to Aspen")
+News.create(title: "Follow John to the best place in Europe", content: "Europe is now better than ever")
 
 puts "Created #{News.count} news"

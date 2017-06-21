3b935bbd734c6bcf13cc028201148d555ce1bbfa
felixottander <felix.ottander@yahoo.com>
2017-06-21 20:15:07 +0200

    test

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,6 +2,8 @@ class News < ApplicationRecord
   validates_presence_of :title, :content
 
   has_attached_file :file
+                    storage: :s3,
+                    s3_credentials: Proc.new{|a| a.instance.s3_credentials }
 
   validates_attachment :file,
                         content_type:

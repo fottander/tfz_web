a29d6e178b5290367a025e618caf5fb4113ed2c7
felixottander <felix.ottander@yahoo.com>
2017-06-21 12:39:02 +0200

    Updates seed file with attachment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 News.delete_all
 
-News.create(title: "Awesome travel insights", content: "Let me tell you about my trip to British Columbia")
-News.create(title: "Great skiing in Aspen", content: "Let me tell you about my trip to Aspen")
-News.create(title: "Follow John to the best place in Europe", content: "Europe is now better than ever")
+News.create(
+  title: "Awesome travel insights",
+  content: "Let me tell you about my trip to British Columbia",
+  file: File.new("#{Rails.root}/spec/images/Agile_workflow.png")
+  )
+
+News.create(
+  title: "Great skiing in Aspen",
+  content: "Let me tell you about my trip to Aspen",
+  file: File.new("#{Rails.root}/spec/images/Agile_workflow.png")
+  )
+
+News.create(
+  title: "Follow John to the best place in Europe",
+  content: "Europe is now better than ever",
+  file: File.new("#{Rails.root}/spec/images/Agile_workflow.png")
+  )
 
 puts "Created #{News.count} news"

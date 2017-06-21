d5a734212184844d85861c57f352ad875a5c8e6c
felixottander <felix.ottander@yahoo.com>
2017-06-21 10:33:30 +0200

    Adds the validations etc in the news model.

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,9 @@
 class News < ApplicationRecord
   validates_presence_of :title, :content
+
+  has_attached_file :file
+
+  validates_attachment :file,
+                        content_type:
+                          {content_type: %w(image/jpg image/jpeg image/png image/gif)}
 end

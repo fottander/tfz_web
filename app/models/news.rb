97922d53e980030fb8330a34b5df34d0a91fc499
felixottander <felix.ottander@yahoo.com>
2017-06-22 09:57:11 +0200

    Adds the credentials in the model.

--- app/models/news.rb
+++ app/models/news.rb
@@ -8,4 +8,15 @@ class News < ApplicationRecord
   validates_attachment :file,
                         content_type:
                           {content_type: %w(image/jpg image/jpeg image/png image/gif)}
+
+  def s3_credentials
+  {
+    bucket: 'tfz',
+    access_key_id: ENV['AWS_ACCESS_KEY'],
+    secret_access_key: ENV['AWS_SECRET_KEY'],
+    s3_region: ENV['AWS_REGION'],
+    url: ':s3_domain_url',
+    s3_host_name: 's3-eu-west-1.amazonaws.com'
+  }
+  end
 end

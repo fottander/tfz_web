74058677ed9c496707da6ec7369d7a72526870b1
felixottander <felix.ottander@yahoo.com>
2017-07-11 16:03:38 +0200

    removes all the unneccessary validartion and file attachmnets in rspec test

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -6,19 +6,9 @@ RSpec.describe News, type: :model do
     it {is_expected.to have_db_column :content}
   end
 
-  describe 'Validations' do
-    it {is_expected.to validate_presence_of :title}
-    it {is_expected.to validate_presence_of :content}
-  end
-
   describe 'Factory' do
     it 'should have valid Factory' do
       expect(create(:news)).to be_valid
-    end
-
-    it 'has a file url by default' do
-      news = create(:news)
-      expect(news.file.url).to include 'Agile_workflow.png'
     end
   end
 end

0c9df27e6a73a68275ad0b48a967b434bd334441
fottander <felix.ottander@yahoo.com>
2017-07-11 16:52:14 +0200

    Get news api (#8)
    * Adds the httparty gem and parsing the api in the view.
    
    * Parsing the file attachment in news index view
    
    * Modifies the controller and the model for news and displays the file on show page
    
    * Refactors the code and deletes unused filea
    
    * Modifies the cucumber test for api calls instead of localhost calls
    
    * REmoves teh file attachmnet in news specs andfactory, deletes seed content.
    
    * removes all the unneccessary validartion and file attachmnets in rspec test
    
    * Truncates the text on index for the news.
    
    * Removes the css

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,27 +1,17 @@
 class NewsController < ApplicationController
+  before_action :set_news, only: [:show]
+
   def index
-    @news = News.all
+    @news = HTTParty.get('https://tfz-backend.herokuapp.com/api/v1/news',
+    :headers =>{'Content-Type' => 'application/json'} )
   end
 
   def show
-    @news = News.find(params[:id])
-  end
-
-  def new
-    @news = News.new
-  end
-
-  def create
-    @news = News.new news_params
-    if @news.save
-      flash[:notice] = 'News article created!'
-      redirect_back(fallback_location: new_news_path)
-    end
   end
 
   private
 
-  def news_params
-    params.require(:news).permit(:title, :content, :file)
+  def set_news
+    @news = News.find(params[:id])
   end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,22 +1,11 @@
+require 'httparty'
+
 class News < ApplicationRecord
-  has_attached_file :file,
-                      storage: :s3,
-                      s3_credentials: Proc.new{|a| a.instance.s3_credentials }
+  include HTTParty
 
-  validates_attachment :file,
-                        content_type:
-                          {content_type: %w(image/jpg image/jpeg image/png image/gif)}
+  base_uri 'https://tfz-backend.herokuapp.com/api/v1'
 
-  def s3_credentials
-  {
-    bucket: 'tfz',
-    access_key_id: ENV['AWS_ACCESS_KEY'],
-    secret_access_key: ENV['AWS_SECRET_KEY'],
-    s3_region: ENV['AWS_REGION'],
-    url: ':s3_domain_url',
-    s3_host_name: 's3-eu-west-1.amazonaws.com'
-  }
+  def self.find(id)
+    get("/news/#{id}")
   end
-
-  validates_presence_of :title, :content
 end
